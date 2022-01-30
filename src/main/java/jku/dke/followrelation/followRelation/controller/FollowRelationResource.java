5c09218cc9874ba001e67a06f7366de1a5029f3f
PRSoftRafael <bakaschevr@gmail.com>
2022-01-30 23:17:27 +0100

    added comments

--- src/main/java/jku/dke/followrelation/followRelation/controller/FollowRelationResource.java
+++ src/main/java/jku/dke/followrelation/followRelation/controller/FollowRelationResource.java
@@ -19,73 +19,91 @@ public class FollowRelationResource {
         this.followRelationService = followRelationService;
     }
 
+    // retrieves all FollowRelationData
+    // this method is only for test purposes
     @GetMapping("/all")
     public ResponseEntity<List<FollowRelationData>> getAllFollowRelationData() {
         List<FollowRelationData> allData = this.followRelationService.getAllFollowRelationData();
         return new ResponseEntity<>(allData, HttpStatus.OK);
     }
 
+    // retrieves all FollowRelationData objects that the current logged-in user follows
     @GetMapping("/follows")
     public ResponseEntity<List<FollowRelationData>> getAllUserFollows(HttpServletRequest request) {
+        // spring check barear token in header
         String jwtToken = request.getHeader("Authorization");
         if(jwtToken == null || (!JwtUtils.isJwtTokenValid(jwtToken))) {
             System.err.println("No authorization-header set or invalid jwtToken provided.");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        // get username from JWT
         String username = JwtUtils.getUsernameFromJwtToken(jwtToken);
         List<FollowRelationData> newList = followRelationService.getAllIFollow(username);
         return new ResponseEntity<>(newList, HttpStatus.OK);
     }
 
+    // retrieves all FollowRelationData objects that follow the current logged-in user
     @GetMapping("/follow")
     public ResponseEntity<List<FollowRelationData>> getAllFollowUser(HttpServletRequest request) {
+        // spring check barear token in header
         String jwtToken = request.getHeader("Authorization");
         if(jwtToken == null || (!JwtUtils.isJwtTokenValid(jwtToken))) {
             System.err.println("No authorization-header set or invalid jwtToken provided.");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        // get username from JWT
         String username = JwtUtils.getUsernameFromJwtToken(jwtToken);
         List<FollowRelationData> newList = followRelationService.getAllFollowMe(username);
         return new ResponseEntity<>(newList, HttpStatus.OK);
     }
 
+    // creates a FollowRelationData object
     @PostMapping("/add")
     public ResponseEntity<FollowRelationData> addFollowRelationData(@RequestBody String username) throws Exception {
         FollowRelationData newFollowRelationData = followRelationService.addFollowRelationData(username);
         return new ResponseEntity<>(newFollowRelationData, HttpStatus.CREATED);
     }
 
+    // creates a follow relation between followRelationData
     @PostMapping("/followRelation")
     public ResponseEntity addFollowRelation(HttpServletRequest request, @RequestBody String user) {
+        // spring check barear token in header
         String jwtToken = request.getHeader("Authorization");
         if(jwtToken == null || (!JwtUtils.isJwtTokenValid(jwtToken))) {
             System.err.println("No authorization-header set or invalid jwtToken provided.");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        // get username from JWT
         String username = JwtUtils.getUsernameFromJwtToken(jwtToken);
         this.followRelationService.addFollowRelation(username, user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // deletes a follow Relation between FollowRelationData
     @DeleteMapping("/deleteFollowRelation/{user}")
     public ResponseEntity removeFollowRelation(HttpServletRequest request, @PathVariable String user) {
+        // spring check barear token in header
         String jwtToken = request.getHeader("Authorization");
         if(jwtToken == null || (!JwtUtils.isJwtTokenValid(jwtToken))) {
             System.err.println("No authorization-header set or invalid jwtToken provided.");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        // get username from JWT
         String username = JwtUtils.getUsernameFromJwtToken(jwtToken);
         this.followRelationService.removeFollowRelation(username, user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // changes the username of a followRelationData object
     @PutMapping("/updateUsername")
     public ResponseEntity updateUsername(HttpServletRequest request, @RequestBody String newUsername) {
+        // spring check barear token in header
         String jwtToken = request.getHeader("Authorization");
         if(jwtToken == null || (!JwtUtils.isJwtTokenValid(jwtToken))) {
             System.err.println("No authorization-header set or invalid jwtToken provided.");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        // get username from JWT
         String username = JwtUtils.getUsernameFromJwtToken(jwtToken);
         this.followRelationService.updateUsername(username, newUsername);
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/jku/dke/followrelation/followRelation/service/FollowRelationService.java
+++ src/main/java/jku/dke/followrelation/followRelation/service/FollowRelationService.java
@@ -14,10 +14,14 @@ public class FollowRelationService {
         this.followRelationRepository = followRelationRepository;
     }
 
+    // search for followRelationData by a username
+    // this method is only for test purposes
     public FollowRelationData getFollowRelationDataByUsername(String username) {
         return this.followRelationRepository.getFollowRelationDataByUsername(username);
     }
 
+    // adds a new followRelationData
+    // throws exception if the given username already exists
     public FollowRelationData addFollowRelationData(String username) throws Exception {
         FollowRelationData newData = followRelationRepository.getFollowRelationDataByUsername(username);
         if (newData != null) {
@@ -26,26 +30,32 @@ public class FollowRelationService {
         return this.followRelationRepository.addFollowRelationData(username);
     }
 
+    // retrieves all the followRelationData objects that the given user follows
     public List<FollowRelationData> getAllIFollow(String username) {
         return this.followRelationRepository.getAllIFollow(username);
     }
 
+    // retrieves all the followRelationData objects that follow the given user
     public List<FollowRelationData> getAllFollowMe(String username) {
         return this.followRelationRepository.getAllFollowMe(username);
     }
 
+    // creates a new follow relation between two users
     public void addFollowRelation(String firstUser, String secondUser){
         this.followRelationRepository.addFollowRelation(firstUser, secondUser);
     }
 
+    // removes a follow relation between two users
     public void removeFollowRelation(String firstUser, String secondUser) {
         this.followRelationRepository.removeFollowRelation(firstUser, secondUser);
     }
 
+    // retrieves all followRelationData objects
     public List<FollowRelationData> getAllFollowRelationData() {
         return this.followRelationRepository.getAllFollowRelationData();
     }
 
+    // updates the username of a user with the 'currentUsername' to 'newUsername'
     public void updateUsername(String currentUsername, String newUsername) {
         this.followRelationRepository.updateUsername(currentUsername, newUsername);
     }
